6a90933bcfbda08afd8c1334ad5918cacd9090ae
Takahiro Furukawa <takapc.study@gmail.com>
2020-05-11 21:16:25 +0900

    no message

---
+++ www/conf/const.production.php
@@ -0,0 +1,54 @@
+<?php
+
+define('MODEL_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../model/');
+define('VIEW_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../view/');
+
+
+define('IMAGE_PATH', '/assets/images/');
+define('STYLESHEET_PATH', '/assets/css/');
+define('IMAGE_DIR', $_SERVER['DOCUMENT_ROOT'] . '/assets/images/' );
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'ec_site');
+define('DB_USER', 'root');
+define('DB_PASS', '6pF7oUwse');
+define('DB_CHARSET', 'utf8');
+
+define('SIGNUP_URL', '/signup.php');
+define('LOGIN_URL', '/login.php');
+define('LOGOUT_URL', '/logout.php');
+define('HOME_URL', '/index.php');
+define('CART_URL', '/cart.php');
+define('FINISH_URL', '/finish.php');
+define('HISTORY_URL', '/history.php');
+define('ADMIN_URL', '/admin.php');
+
+define('REGEXP_ALPHANUMERIC', '/\A[0-9a-zA-Z]+\z/');
+define('REGEXP_POSITIVE_INTEGER', '/\A([1-9][0-9]*|0)\z/');
+
+// 商品一覧1ページの最大表示件数
+define('INDEX_NUM_MAX', 8);
+
+define('USER_NAME_LENGTH_MIN', 6);
+define('USER_NAME_LENGTH_MAX', 100);
+define('USER_PASSWORD_LENGTH_MIN', 6);
+define('USER_PASSWORD_LENGTH_MAX', 100);
+
+define('USER_TYPE_ADMIN', 1);
+define('USER_TYPE_NORMAL', 2);
+
+define('ITEM_NAME_LENGTH_MIN', 1);
+define('ITEM_NAME_LENGTH_MAX', 100);
+
+define('ITEM_STATUS_OPEN', 1);
+define('ITEM_STATUS_CLOSE', 0);
+
+define('PERMITTED_ITEM_STATUSES', array(
+  'open' => 1,
+  'close' => 0,
+));
+
+define('PERMITTED_IMAGE_TYPES', array(
+  IMAGETYPE_JPEG => 'jpg',
+  IMAGETYPE_PNG => 'png',
+));
