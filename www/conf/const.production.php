37e60ca89c5b3a2c0a804d6b0e93e0d738c19ebd
Takahiro Furukawa <takapc.study@gmail.com>
2020-06-01 23:27:26 +0900

    本番環境用のconfを設定 ページングのリンク修正

--- www/conf/const.production.php
+++ www/conf/const.production.php
@@ -1,12 +1,12 @@
 <?php
 
-define('MODEL_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../model/');
-define('VIEW_PATH', $_SERVER['DOCUMENT_ROOT'] . '/../view/');
+define('MODEL_PATH', $_SERVER['DOCUMENT_ROOT'] . '/model/');
+define('VIEW_PATH', $_SERVER['DOCUMENT_ROOT'] . '/view/');
 
 
-define('IMAGE_PATH', '/assets/images/');
-define('STYLESHEET_PATH', '/assets/css/');
-define('IMAGE_DIR', $_SERVER['DOCUMENT_ROOT'] . '/assets/images/' );
+define('IMAGE_PATH', '/veggieshop/assets/images/');
+define('STYLESHEET_PATH', '/veggieshop/assets/css/');
+define('IMAGE_DIR', $_SERVER['DOCUMENT_ROOT'] . '/veggieshop/assets/images/' );
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'ec_site');
@@ -14,14 +14,14 @@ define('DB_USER', 'root');
 define('DB_PASS', '6pF7oUwse');
 define('DB_CHARSET', 'utf8');
 
-define('SIGNUP_URL', '/signup.php');
-define('LOGIN_URL', '/login.php');
-define('LOGOUT_URL', '/logout.php');
-define('HOME_URL', '/index.php');
-define('CART_URL', '/cart.php');
-define('FINISH_URL', '/finish.php');
-define('HISTORY_URL', '/history.php');
-define('ADMIN_URL', '/admin.php');
+define('SIGNUP_URL', '/veggieshop/signup.php');
+define('LOGIN_URL', '/veggieshop/login.php');
+define('LOGOUT_URL', '/veggieshop/logout.php');
+define('HOME_URL', '/veggieshop/index.php');
+define('CART_URL', '/veggieshop/cart.php');
+define('FINISH_URL', '/veggieshop/finish.php');
+define('HISTORY_URL', '/veggieshop/history.php');
+define('ADMIN_URL', '/veggieshop/admin.php');
 
 define('REGEXP_ALPHANUMERIC', '/\A[0-9a-zA-Z]+\z/');
 define('REGEXP_POSITIVE_INTEGER', '/\A([1-9][0-9]*|0)\z/');
